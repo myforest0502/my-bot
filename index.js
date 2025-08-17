7cc976114817fc353195316ca258de60e70c7332
forest <muforest2022@gmail.com>
2025-08-17 23:17:52 +0900

    Render対応：戦国Bot起動処理を追加

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("こんにちは、戦国Botです!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Bot is listening on port ${PORT}`);
+});
